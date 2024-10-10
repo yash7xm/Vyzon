555e6ec0b7ec2e05b6a0c2b53667bea19a0ce8fd
yash7xm <yash.7xm@gmail.com>
2024-10-10 12:45:06 +0530

    class program eg added

--- Program.js
+++ Program.js
@@ -264,6 +264,32 @@ write(result);
 // }
 // write(foo(2));
 
+// classes
+// class Point {
+//     let x, y;
+
+//     def constructor(x, y) {
+//         this.x = x;
+//         this.y = y;
+//     }
+// }
+
+// class Point3D extends Point {
+//     let z;
+
+//     def constructor(x, y, z) {
+//         super(x,y);
+//         this.z = z;
+//     }
+
+//     def sum() {
+//         write(this.x + this.y + this.z);
+//     }
+// }
+
+// let point = new Point3D(10,20,30);
+// point.sum();
+
 
 `
 
